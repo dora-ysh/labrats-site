fbd6c871551d61d4633a4fa772f673db4dd830b4
dora <dora.ysh06@gmail.com>
2026-05-22 22:56:22 -0700

    add deploy workflow!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dora-ysh.github.io',
+	base: '/labrats-site',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
